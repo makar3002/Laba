bfdc6627e6c1728e2f5f3fb9b4e0cdb2e89fc497
makar3002 <makarenko.ninkita.1999@gmail.com>
2019-10-02 10:49:08 +0400

    Auth complete

--- index.php
+++ index.php
@@ -1,23 +1,46 @@
 <!DOCTYPE html>
 <html lang="ru">
-  <head>
-    <title>Автомобильный гараж</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/bootstrap.css">
-  </head>
-  <body>
+    <head>
+        <title>Автомобильный гараж</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+    <?php
+    session_start();
+    ?>
     <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 border-bottom page-header">
-      <h5 class="my-0 mr-md-auto font-weight-normal text-light">ГаражQ</h5>
-      <nav class="my-2 my-md-0 mr-md-3">
+        <h5 class="my-0 mr-md-auto font-weight-normal text-light">ГаражQ</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
         <a class="p-2 text-light" href="autos.html">Автомобили</a>
         <a class="p-2 text-light" href="owners.html">Владельцы</a>
         <a class="p-2 text-light" href="#">Список сторожей</a>
-        <a class="p-2 text-light" href="#">Журнал</a>
-      </nav>
-      <div class="col-4 d-flex justify-content-end align-items-center">
-        <a class="btn btn-sm btn-outline-secondary " href="auth.php">Sign up</a>
-      </div>
+            <?php
+            if (!isset($_SESSION['email'])){
+                ?>
+                <a class="p-2 text-light" <?php echo 'href="#"' ?> > Журнал</a>
+                <?php
+            } else {
+                ?>
+                <a class="p-2 text-light" <?php echo 'href="journal.php"' ?> > Журнал</a>
+                <?php
+            }
+            ?>
+        </nav>
+        <div class="col-4 d-flex justify-content-end align-items-center">
+            <?php
+            if (!isset($_SESSION['email'])){
+                ?>
+                <a class="btn btn-sm btn-outline-secondary " <?php echo "href='auth.php'" ?> > Sign up</a>
+                <?php
+            } else {
+                ?>
+                <a class="btn btn-sm btn-outline-secondary " <?php echo "href='php/logout.php'" ?> > Sign out</a>
+                <?php
+            }
+            ?>
+        </div>
     </header>
     <main>
       <article class="entry">
@@ -35,9 +58,9 @@
           <p>///...///</p>
 
         </section>
-        
+
       </article>
-    
+
     <section class="photos">
       <figure class="slides">
         <img src="images/1.jpg" alt="Машина 1">
@@ -53,5 +76,5 @@
       </section>
     </footer>
     <div class="podfooter"></div>
-  </body>
+    </body>
 </html>
